5839cc96b5824d1222d27127a11aeff828d181ac
Maurice van Cooten <maurice@minus.nz>
2016-06-21 08:49:27 +1200

    formatted code

--- hamburger-nav/custom.js
+++ hamburger-nav/custom.js
@@ -1,18 +1,18 @@
 var nav = document.querySelector('.nav-collapse'),
-navList = nav.firstElementChild,
-link = document.createElement('a');
-					
+  navList = nav.firstElementChild,
+  link = document.createElement('a');
+
 nav.appendChild(link);
 link.classList.add('hamburger');
 link.innerHTML = '<span>Menu</span>';
-					
-var	hamburger = document.querySelector('.hamburger');
+
+var hamburger = document.querySelector('.hamburger');
 
 navList.classList.add('accessibly-hidden');
 
-hamburger.addEventListener('click', function (e) {
+hamburger.addEventListener('click', function(e) {
 
-	e.preventDefault();
-	navList.classList.toggle('accessibly-hidden');
+  e.preventDefault();
+  navList.classList.toggle('accessibly-hidden');
 
 });
