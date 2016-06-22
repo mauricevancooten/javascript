33300d738a304742a235a7bd98aeb9b90ed1bace
Maurice van Cooten <maurice@minus.nz>
2016-06-23 11:26:10 +1200

    combined fade in with setTimeout

--- fade-in/custom.js
+++ fade-in/custom.js
@@ -1,5 +1,15 @@
-var el = document.querySelector('.js-fade');
+var el = document.querySelector('header'),
+  el2 = document.querySelector('.wrapper'),
+  el3 = document.querySelector('footer');
 
-window.addEventListener('scroll', function() {
+setTimeout(function() {
   el.classList.remove('is-paused');
-});
+}, 800);
+
+setTimeout(function() {
+  el2.classList.remove('is-paused');
+}, 1600);
+
+setTimeout(function() {
+  el3.classList.remove('is-paused');
+}, 2400);
