f1b9b4f9653c74d6ee5bfa5ea4e16ada69d48f91
Maurice van Cooten <maurice@minus.nz>
2016-09-29 13:08:26 +1300

    set href attribute

--- hamburger-nav/custom.js
+++ hamburger-nav/custom.js
@@ -3,16 +3,17 @@ var nav = document.querySelector('.nav-collapse'),
   link = document.createElement('a');
 
 nav.appendChild(link);
+link.setAttribute('href', '#');
 link.classList.add('hamburger');
 link.innerHTML = '<span>Menu</span>';
 
 var hamburger = document.querySelector('.hamburger');
 
-navList.classList.add('accessibly-hidden');
+navList.classList.add('hide-text');
 
 hamburger.addEventListener('click', function(e) {
 
   e.preventDefault();
-  navList.classList.toggle('accessibly-hidden');
+  navList.classList.toggle('hide-text');
 
 });
