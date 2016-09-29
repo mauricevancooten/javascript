68a129899133710b3c2c02ef2b91859ef28f886d
Maurice van Cooten <maurice@minus.nz>
2016-09-29 18:43:56 +1300

    added animation

--- event-listener/custom.js
+++ event-listener/custom.js
@@ -1,7 +1,9 @@
 var el = document.querySelector('h1'),
 button = document.querySelector('.show');
 
+
 el.classList.add('hide-text');
+el.style.transition = '1s';
 
 button.addEventListener('click', function(){
 
