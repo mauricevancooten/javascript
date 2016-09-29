2fa40252b9eaef4f45952cf1c79e45c486999f1c
Maurice van Cooten <maurice@minus.nz>
2016-09-29 20:26:18 +1300

    works with js turned off

--- accordion/custom.js
+++ accordion/custom.js
@@ -2,15 +2,19 @@ var accordion = document.querySelectorAll('.accordion'),
     content = document.querySelector('.content'),
     allContent = document.querySelectorAll('.content');
 
+for (var i = 0; i < allContent.length; i++) {
+    allContent[i].style.maxHeight = 0;
+}
+
 for (var i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function() {
         for (var i = 0; i < allContent.length; i++) {
-            allContent[i].classList.remove('show');
+            allContent[i].style.maxHeight = 0;
         }
         for (var i = 0; i < accordion.length; i++) {
             accordion[i].classList.remove('active');
         }
         this.classList.add('active');
-        this.nextElementSibling.classList.add('show');
+        this.nextElementSibling.style.maxHeight = '1000px';
     });
 };
