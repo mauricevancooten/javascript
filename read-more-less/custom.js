67a9a0e515390445526fc9bea28b643bcbe8fc3e
Maurice van Cooten <maurice@minus.nz>
2016-09-29 14:57:59 +1300

    animated read more example

--- read-more-less/custom.js
+++ read-more-less/custom.js
@@ -2,7 +2,7 @@ var button = document.querySelector('.show-more'),
 more = document.querySelector('.more'),
 visible = false;
 
-more.classList.add('hide-text');
+more.style.maxHeight = 0;
 
 button.addEventListener('click', function () {
 
@@ -10,8 +10,10 @@ button.addEventListener('click', function () {
 
 	if (!visible) {
 		button.innerHTML = 'Show less...';
+		more.style.maxHeight = 500 + 'px';
 	} else {
 		button.innerHTML = 'Read more...';
+		more.style.maxHeight = 0;
 	}
 
 	visible = !visible;
