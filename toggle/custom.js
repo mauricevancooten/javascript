2af22a08d2fe2d7e7017d905b2a40c096781f033
Maurice van Cooten <maurice@minus.nz>
2016-09-29 18:55:25 +1300

    add transition after content loaded

--- toggle/custom.js
+++ toggle/custom.js
@@ -2,10 +2,11 @@ var button = document.querySelector('.show'),
 text = document.querySelector('.enquiries');
 
 text.classList.add('hide-text');
-text.style.transition = '1s';
+
+
 
 button.addEventListener('click', function(){
-						
+	text.style.transition = '1s';					
 	text.classList.toggle('hide-text');
 
 });
