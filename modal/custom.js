16d2388a7c640912b8a4166dff850f9fa7616f17
Maurice van Cooten <maurice@minus.nz>
2016-09-29 13:48:08 +1300

    better accessibility with javascript turned off

--- modal/custom.js
+++ modal/custom.js
@@ -1,19 +1,28 @@
-var open = document.querySelector('.show'),
-close = document.querySelector('.hide'),
-modal = document.querySelector('.overlay'),
-html = document.querySelector('html');	
-					
+var open = document.createElement('button'),
+    close = document.createElement('button'),
+    modal = document.querySelector('.overlay'),
+    modalContent = document.querySelector('.overlay-content'),
+    html = document.querySelector('html');
+
+open.classList.add('.show');
+open.innerHTML = 'Open';
+close.classList.add('.close');
+close.innerHTML = 'Close';
+
+modalContent.appendChild(close);
+modal.parentNode.insertBefore(open, modal);
+
 html.classList.add('js');
 modal.classList.add('hide-text');
 
-close.addEventListener('click', function(){
+close.addEventListener('click', function() {
 
-	modal.classList.add('hide-text');
-					
+    modal.classList.add('hide-text');
+
 });
 
-open.addEventListener('click', function(){
-					
-	modal.classList.remove("hide-text");
-					
+open.addEventListener('click', function() {
+
+    modal.classList.remove("hide-text");
+
 });
