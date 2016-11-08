df747d13e999b971335e4117d9f50afd9ac964c1
Maurice van Cooten <maurice@minus.nz>
2016-11-08 14:28:26 +1300

    simpler fade works with JS turned off

--- fade-in/custom.js
+++ fade-in/custom.js
@@ -2,14 +2,18 @@ var el = document.querySelector('header'),
   el2 = document.querySelector('.wrapper'),
   el3 = document.querySelector('footer');
 
+el.style.cssText = 'opacity: 0; transition: 1s';
+el2.style.cssText = 'opacity: 0; transition: 1s';
+el3.style.cssText = 'opacity: 0; transition: 1s';
+
 setTimeout(function() {
-  el.classList.remove('is-paused');
+  el.style.opacity = 1;
 }, 800);
 
 setTimeout(function() {
-  el2.classList.remove('is-paused');
+  el2.style.opacity = 1;
 }, 1600);
 
 setTimeout(function() {
-  el3.classList.remove('is-paused');
+  el3.style.opacity = 1;
 }, 2400);
