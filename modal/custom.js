66ad88b6aea11d31cc736f3757514e4bc9f300fe
Maurice van Cooten <maurice@minus.nz>
2016-11-08 15:36:50 +1300

    indenting

--- modal/custom.js
+++ modal/custom.js
@@ -1,8 +1,8 @@
 var open = document.createElement('button'),
-    close = document.createElement('button'),
-    modal = document.querySelector('.overlay'),
-    modalContent = document.querySelector('.overlay-content'),
-    html = document.querySelector('html');
+  close = document.createElement('button'),
+  modal = document.querySelector('.overlay'),
+  modalContent = document.querySelector('.overlay-content'),
+  html = document.querySelector('html');
 
 open.classList.add('.show');
 open.innerHTML = 'Open';
@@ -17,12 +17,12 @@ modal.classList.add('hide-text');
 
 close.addEventListener('click', function() {
 
-    modal.classList.add('hide-text');
+  modal.classList.add('hide-text');
 
 });
 
 open.addEventListener('click', function() {
 
-    modal.classList.remove("hide-text");
+  modal.classList.remove("hide-text");
 
 });
