0ecba6c3a9b4f2ec5f62d290f38c8a8381fd1e61
Maurice van Cooten <maurice@minus.nz>
2016-11-11 11:36:00 +1300

    tidied up code

--- randomise/custom.js
+++ randomise/custom.js
@@ -1,8 +1,13 @@
-var button = document.querySelector('.randomise'),
-box = document.querySelector('.example');
+var button, box, randomRad, randomHex, randomDeg
 
+button = document.querySelector('.randomise')
+box = document.querySelector('.box')
 
-button.addEventListener('click', function () {
-	var random = Math.floor(Math.random() * 256);
-	box.style.backgroundColor = 'rgba(255,'+ random + ','+ random + ',.8)'
-});
+button.addEventListener('click', function() {
+  // Random number within range of 50 / 360
+  randomRad = Math.floor(Math.random() * 50)
+  randomDeg = Math.floor(Math.random() * 360)
+    // (16777215 == ffffff in decimal)
+  randomHex = Math.floor(Math.random() * 16777215).toString(16)
+  box.style.cssText = 'border-radius:' + randomRad + '%; transform: rotate(' + randomDeg + 'deg); background-color: #' + randomHex + ';'
+})
