2054ba5cb9726193244748eebc67cf3be59a95e6
Maurice van Cooten <maurice@minus.nz>
2016-11-11 09:16:28 +1300

    commented code

--- document-ready/custom.js
+++ document-ready/custom.js
@@ -1,3 +1,4 @@
- document.addEventListener('DOMContentLoaded', function() {
+// Run script once DOM content has loaded
+document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('h1').innerHTML = 'DOM Content Loaded'
 })
