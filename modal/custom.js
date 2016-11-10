ed102b1eb4483a80fac865d2311d1b553353973e
Maurice van Cooten <maurice@minus.nz>
2016-11-11 09:41:28 +1300

    tidied up code

--- modal/custom.js
+++ modal/custom.js
@@ -1,28 +1,31 @@
-var open = document.createElement('button'),
-  close = document.createElement('button'),
-  modal = document.querySelector('.overlay'),
-  modalContent = document.querySelector('.overlay-content'),
-  html = document.querySelector('html');
+var open, close, modal, modalContent, html
 
-open.classList.add('.show');
-open.innerHTML = 'Open';
-close.classList.add('.close');
-close.innerHTML = 'Close';
+modal = document.querySelector('.overlay')
+modalContent = document.querySelector('.overlay-content')
+html = document.querySelector('html')
+// Create button elements
+open = document.createElement('button')
+close = document.createElement('button')
 
-modalContent.appendChild(close);
-modal.parentNode.insertBefore(open, modal);
+// Add classes and text to buttons
+open.classList.add('.show')
+open.innerHTML = 'Open'
+close.classList.add('.close')
+close.innerHTML = 'Close'
 
-html.classList.add('js');
-modal.classList.add('hide-text');
+// Insert close buton inside .overlay-content element
+modalContent.appendChild(close)
+// Insert open button after .overlay element
+modal.parentNode.insertBefore(open, modal) 
+// Add js class to html element for accessibility (see CSS)
+html.classList.add('js')
+
+modal.classList.add('hide-text')
 
 close.addEventListener('click', function() {
-
-  modal.classList.add('hide-text');
-
+  modal.classList.add('hide-text')
 });
 
 open.addEventListener('click', function() {
-
-  modal.classList.remove("hide-text");
-
+  modal.classList.remove("hide-text")
 });
