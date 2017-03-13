5ee574a6136dbfd0ba0f603baa46e7a51560bba6
Maurice van Cooten <maurice@minus.nz>
2017-03-13 17:29:34 +1300

    tweaked progress on initial state

--- scroll-progress-bar/custom.js
+++ scroll-progress-bar/custom.js
@@ -5,7 +5,7 @@ body = document.querySelector('body')
 height = body.clientHeight
 percentage = 0
 
-scrollBar.style.width = percentage + "%"
+scrollBar.style.width = ((this.innerHeight / height)) * 100 + "%"
 
 window.addEventListener('scroll', function() {
   percentage = ((this.pageYOffset + this.innerHeight) / height) * 100
