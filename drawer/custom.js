6bc9f7e24f3df48c97a98050ca7374d6ec8e898f
Maurice van Cooten <maurice@minus.nz>
2017-04-04 15:05:41 +1200

    if JS is turned off improved code

--- drawer/custom.js
+++ drawer/custom.js
@@ -1,14 +1,38 @@
-var menu, close, nav
-menu = document.querySelector('.hamburger')
-close = document.querySelector('.close')
+'use strict'
+
+var menu, close, drawer, nav, html
+
+drawer = document.querySelector('.sticky')
 nav = document.querySelector('nav')
+html = document.querySelector('html')
 
-nav.style.width = 0
+// Create link elements, set attributes, classes and content
+menu = document.createElement('a')
+close = document.createElement('a')
+menu.setAttribute('href','#')
+close.setAttribute('href','#')
+menu.classList.add('menu')
+close.classList.add('close')
+menu.innerHTML = '<span>Menu</span>'
+close.innerHTML = '<span>Close</span>'
 
-menu.addEventListener('click', function() {
-  nav.style.width = 250 + 'px'
+// Append links before drawer and nav
+drawer.parentNode.insertBefore(menu, drawer)
+nav.parentNode.insertBefore(close, nav)
+
+menu = document.querySelector('.menu')
+close = document.querySelector('.close')
+
+drawer.style.width = 0
+
+menu.addEventListener('click', function(e) {
+	e.preventDefault()
+  drawer.style.width = '250px'
 })
 
-close.addEventListener('click', function() {
-  nav.style.width = 0
+close.addEventListener('click', function(e) {
+	e.preventDefault(e)
+  drawer.style.width = 0
 })
+
+html.classList.add('js')
