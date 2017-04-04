b6539690b77b492984d734318e1772fdce25a154
Maurice van Cooten <maurice@minus.nz>
2017-04-04 15:35:32 +1200

    strict mode, removed extra link, accurate scroll to start

--- scroll-progress-bar/custom.js
+++ scroll-progress-bar/custom.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var scrollBar, body, height, percentage
 
 scrollBar = document.querySelector('.scroll-bar')
@@ -5,7 +7,7 @@ body = document.querySelector('body')
 height = body.clientHeight
 percentage = 0
 
-scrollBar.style.width = percentage + "%"
+scrollBar.style.width = ((this.innerHeight / height)) * 100 + "%"
 
 window.addEventListener('scroll', function() {
   percentage = ((this.pageYOffset + this.innerHeight) / height) * 100
