c06dac5a4dc94efe0b81835ee3a33b19c0f00e82
Maurice van Cooten <maurice@minus.nz>
2017-04-05 13:54:35 +1200

    stack gallery using z-index

---
+++ stack-gallery/custom.js
@@ -0,0 +1,11 @@
+var image, count
+
+image = document.querySelector('.gallery').querySelectorAll('img')
+count = 0;
+
+for (var i = 0; i < image.length; i++) {
+	image[i].addEventListener('click', function () {
+		count++;
+		this.style.zIndex = count;
+	})
+}
