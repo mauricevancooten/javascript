f69e57a7e00ee81e8bfaf38140de2db01aabdcfe
Maurice van Cooten <maurice@minus.nz>
2017-07-09 15:36:49 +1200

    use const instead of var

--- toggle/custom.js
+++ toggle/custom.js
@@ -1,14 +1,11 @@
 'use strict'
 
-var button, content
-
-button = document.querySelector('.show')
-content = document.querySelector('.enquiries')
+const link = document.querySelector('.show')
+const content = document.querySelector('.enquiries')
 
 content.classList.add('hide-text')
 
-button.addEventListener('click', function(e){
+link.addEventListener('click', function(e){
 	e.preventDefault() // Prevent default behaviour
-	content.style.transition = '1s'					
 	content.classList.toggle('hide-text')
 })
