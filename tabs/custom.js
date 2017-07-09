d5dc7abf76424204b229c2b061e898c377dfe91a
Maurice van Cooten <maurice@minus.nz>
2017-07-09 15:38:50 +1200

    use const instead of var

--- tabs/custom.js
+++ tabs/custom.js
@@ -1,33 +1,33 @@
 'use strict'
 
-var tabContent, tabButton, tabId
+const tabContent = document.querySelector('#tabs').querySelectorAll('div')
+const tabButton = document.querySelector('#tabs').childNodes[1].querySelectorAll('a')
 
-tabContent = document.querySelector('#tabs').querySelectorAll('div')
-tabButton = document.querySelector('#tabs').childNodes[1].querySelectorAll('a')
-console.log(tabButton)
-  // Hide all tabContent except the first
+// Hide all tabContent except the first
 for (var i = 0; i < tabContent.length; i++) {
   if (i > 0) {
     tabContent[i].classList.add('hide-text')
   }
 }
+
 // Loop through tabButton array
 for (var i = 0; i < tabButton.length; i++) {
-  // Set first tab to active state
+  // Set first tab to active class
   tabButton[0].classList.add('active')
 
   tabButton[i].addEventListener('click', function(e) {
     e.preventDefault()
+    // Remove active class on all tabs
     for (var i = 0; i < tabButton.length; i++) {
       tabButton[i].classList.remove('active')
     }
-    // Add active state for tab clicked on
+    // Add active class for tab clicked on
     this.classList.add('active')
     for (var i = 0; i < tabContent.length; i++) {
       tabContent[i].classList.add('hide-text')
     }
     // Get the href value of the clicked item
-    tabId = this.getAttribute('href')
+    var tabId = this.getAttribute('href')
     tabId = document.querySelector(tabId)
     tabId.classList.remove('hide-text')
   })
