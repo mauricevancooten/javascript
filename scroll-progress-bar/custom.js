6a3bfd8e5d9714e5649fc2ac6d733384f6581da6
Maurice van Cooten <maurice@minus.nz>
2017-07-11 18:21:43 +1200

    consitency with quatations

--- scroll-progress-bar/custom.js
+++ scroll-progress-bar/custom.js
@@ -5,9 +5,9 @@ const body = document.querySelector('body')
 const height = body.clientHeight
 var percentage = 0
 
-scrollBar.style.width = ((window.innerHeight / height)) * 100 + "%"
+scrollBar.style.width = ((window.innerHeight / height)) * 100 + '%'
 
 window.addEventListener('scroll', function() {
   percentage = ((this.pageYOffset + this.innerHeight) / height) * 100
-  scrollBar.style.width = percentage + "%"
+  scrollBar.style.width = percentage + '%'
 })
