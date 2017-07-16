9bfeb721bf164d22dd6d7b7af27478291385b139
Maurice van Cooten <maurice@minus.nz>
2017-07-16 20:17:13 +1200

    changed button to btn

--- event-listener/custom.js
+++ event-listener/custom.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const el = document.querySelector('h1')
-const button = document.querySelector('.show')
+const btn = document.querySelector('.show')
 
 el.classList.add('hide-text')
 el.style.transition = '1s'
 
 // Listens for click event before triggering script
-button.addEventListener('click', function() {
+btn.addEventListener('click', function() {
   el.classList.remove('hide-text')
 })
