4e28419c8188fac6be513e649f58cbb88b262dee
Maurice van Cooten <maurice@minus.nz>
2017-07-16 20:18:16 +1200

    redesigned example to be more purposeful

--- gallery-commission-calculator/custom.js
+++ gallery-commission-calculator/custom.js
@@ -1,29 +1,19 @@
-var price, commission, galleryFee, total, button, galleryPrice, galleryCommission
+'use strict'
 
-price = document.querySelector('.price')
-commission = document.querySelector('.commission')
-galleryFee = document.querySelector('.gallery-fee')
-total = document.querySelector('.total')
-button = document.querySelector('.calculate')
-  // Calculate the gallery commission
-function galleryCommission(artWorkPrice) {
-  var total = artWorkPrice * (commission.value / 100)
-  return total
-}
-// Calculate the total including the gallery commission
-function galleryTotal(artWorkPrice) {
-  var total = artWorkPrice * (1 + (commission.value / 100))
+const galleryPrice = document.querySelector('.price')
+const galleryPercentage = document.querySelector('.percent')
+const galleryCommission = document.querySelector('.commission')
+const artistsTotal = document.querySelector('.total')
+const btn = document.querySelector('.calc')
+
+function commissionCalc(price) {
+  const total = price * (galleryPercentage.value / 100)
   return total
 }
 
-button.addEventListener('click', function() {
-  galleryCommission = galleryCommission(price.value)
-  galleryCommission = Math.round(galleryCommission * 10) / 10 // Rounding
-  galleryCommission = galleryCommission.toFixed(2) // Two decimal places
-  galleryFee.innerHTML = galleryCommission
-  galleryPrice = galleryTotal(price.value)
-  galleryPrice = Math.round(galleryPrice * 10) / 10
-  galleryPrice = galleryPrice.toFixed(2)
-  total.innerHTML = galleryPrice
-  console.log(galleryPrice)
+btn.addEventListener('click', function () {
+  const commission = commissionCalc(galleryPrice.value)
+  galleryCommission.innerHTML = Math.round(commission * 100) / 100
+  const artistsPrice = galleryPrice.value - commission
+  artistsTotal.innerHTML = artistsPrice.toFixed(2)
 })
