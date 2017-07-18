87c0482fa8b7d14f1d0c22ad7796415dd4ca5f12
Maurice van Cooten <maurice@minus.nz>
2017-07-18 18:00:20 +1200

    two parameters on the function

--- gallery-commission-calculator/custom.js
+++ gallery-commission-calculator/custom.js
@@ -6,13 +6,13 @@ const galleryCommission = document.querySelector('.commission')
 const artistsTotal = document.querySelector('.total')
 const btn = document.querySelector('.calc')
 
-function commissionCalc(price) {
-  const total = price * (galleryPercentage.value / 100)
+function commissionCalc(price, percent) {
+  const total = price * (percent / 100)
   return total
 }
 
 btn.addEventListener('click', function () {
-  const commission = commissionCalc(galleryPrice.value)
+  const commission = commissionCalc(galleryPrice.value, galleryPercentage.value)
   galleryCommission.innerHTML = Math.round(commission * 100) / 100
   const artistsPrice = galleryPrice.value - commission
   artistsTotal.innerHTML = artistsPrice.toFixed(2)
--- gallery-price-calculator/custom.js
+++ gallery-price-calculator/custom.js
@@ -6,13 +6,13 @@ const galleryCommission = document.querySelector('.commission')
 const galleryTotal = document.querySelector('.total')
 const btn = document.querySelector('.calc')
 
-function commissionCalc(price) {
-  const total = price * (1 + ((galleryPercentage.value / (1 - galleryPercentage.value / 100)) / 100))
+function commissionCalc(price, percent) {
+  const total = price * (1 + ( percent / (1 - percent / 100) / 100))
   return total
 }
 
 btn.addEventListener('click', function() {
-  const galleryPrice = commissionCalc(artistsPrice.value)
+  const galleryPrice = commissionCalc(artistsPrice.value, galleryPercentage.value)
   const commission = Math.round((galleryPrice - artistsPrice.value) * 100) / 100
   galleryCommission.innerHTML = commission.toFixed(2)
   galleryTotal.innerHTML = galleryPrice.toFixed(2)
