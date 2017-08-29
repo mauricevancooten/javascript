d1b95a6bdbd0953e60e0e4625879ed0a2d934757
Maurice van Cooten <maurice@minus.nz>
2017-08-30 11:52:25 +1200

    renamed folder and added template literal, arrow function

--- randomiser/custom.js
+++ randomiser/custom.js
@@ -1,14 +1,15 @@
 'use strict'
 
-const btn = document.querySelector('.randomise')
-const box = document.querySelector('.box')
+const btn = document.getElementById('randomiser')
+const box = document.getElementById('box')
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', () => {
   // Random number within range of 50
   const randomRad = Math.floor(Math.random() * 50)
   // Random number within range of 360
   const randomDeg = Math.floor(Math.random() * 360)
   // (16777215 == ffffff in decimal)
   const randomHex = Math.floor(Math.random() * 16777215).toString(16)
-  box.style.cssText = 'border-radius:' + randomRad + '%; transform: rotate(' + randomDeg + 'deg); background-color: #' + randomHex + ';'
+  // Add CSS
+  box.style.cssText = `border-radius: ${randomRad}%; transform: rotate(${randomDeg}deg); background-color: #${randomHex};`
 })
