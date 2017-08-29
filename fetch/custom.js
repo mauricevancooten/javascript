9f2f646f7eb7972550b61d6b9a67d6d4f1d08a39
Maurice van Cooten <maurice@minus.nz>
2017-08-30 11:50:53 +1200

    replaced fetch-json and fetch-html

---
+++ fetch/custom.js
@@ -0,0 +1,26 @@
+fetch('data.json')
+.then( (res) => {
+    if (res.status !== 200) {
+      console.log('Looks like there was a problem: ' + res.status)
+      return
+    }
+    // Examine the text in the response
+    res.json()
+    .then( (data) => {
+      // Build up string with new content.
+      let newContent = ''
+
+      data.artists.map( (artist) => {
+        newContent += `<div><img src="${artist.work}" alt="${artist.title}">`
+        newContent += `<p><strong>${artist.name}</strong><br>${artist.title}<br>`
+        newContent += `<span>${artist.medium}</span></p></div>`
+      })
+
+      // Update the page with the new content
+      document.getElementById('content').innerHTML = newContent
+    })
+  }
+)
+.catch( (err) => {
+  console.log('Fetch Error: ' + err)
+})
