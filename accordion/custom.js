5f39472caf98c96c845a6b756a72b780fee41d08
Maurice van Cooten <maurice@minus.nz>
2017-08-30 11:53:23 +1200

    seperate callback function

--- accordion/custom.js
+++ accordion/custom.js
@@ -1,21 +1,40 @@
 'use strict'
 
-const accordion = document.querySelectorAll('.accordion')
-const content = document.querySelectorAll('.content')
+const accordion = document.getElementsByClassName('accordion')
+const content = document.getElementsByClassName('content')
 
-for (var i = 0; i < content.length; i++) {
+// Loop through .content blocks and set max height to hide.
+for (let i = 0; i < content.length; i++) {
   content[i].style.maxHeight = 0
 }
 
-for (var i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener('click', function() {
-    for (var i = 0; i < content.length; i++) {
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', showAccordion)
+}
+
+function showAccordion() {
+  // Check to see if content is open ( maxHeight === 1000px)
+  if (this.nextElementSibling.style.maxHeight === '1000px') {
+    // Loop through .content blocks and set max height to hide.
+    for (let i = 0; i < content.length; i++) {
       content[i].style.maxHeight = 0
     }
-    for (var i = 0; i < accordion.length; i++) {
+    // Removes active class of each accordion button
+    for (let i = 0; i < accordion.length; i++) {
       accordion[i].classList.remove('active')
     }
+  } else { // Else if content is closed (maxHeight !== 1000px)
+    // Loop through .content blocks and set max height to hide.
+    for (let i = 0; i < content.length; i++) {
+      content[i].style.maxHeight = 0
+    }
+    // Removes active class of each accordion button
+    for (let i = 0; i < accordion.length; i++) {
+      accordion[i].classList.remove('active')
+    }
+    // Adds active class to accordion button clicked on (this)
     this.classList.add('active')
+    // Opens (sibling), content beneat through max-height CSS
     this.nextElementSibling.style.maxHeight = '1000px'
-  })
+  } // End if else
 }
