3fdaca1e7cbf22dbd23fd2604f293d7b6ad4bd47
Maurice van Cooten <maurice@minus.nz>
2017-08-30 13:34:13 +1200

    arrow function, getElementById instead of querySelector

--- gallery-commission-calculator/custom.js
+++ gallery-commission-calculator/custom.js
@@ -1,17 +1,17 @@
 'use strict'
 
-const galleryPrice = document.querySelector('.price')
-const galleryPercentage = document.querySelector('.percent')
-const galleryCommission = document.querySelector('.commission')
-const artistsTotal = document.querySelector('.total')
-const btn = document.querySelector('.calc')
+const galleryPrice = document.getElementById('price')
+const galleryPercentage = document.getElementById('percent')
+const galleryCommission = document.getElementById('commission')
+const artistsTotal = document.getElementById('total')
+const btn = document.getElementById('calc')
 
 function commissionCalc(price, percent) {
   const total = price * (percent / 100)
   return total
 }
 
-btn.addEventListener('click', function () {
+btn.addEventListener('click', () => {
   const commission = commissionCalc(galleryPrice.value, galleryPercentage.value)
   galleryCommission.innerHTML = Math.round(commission * 100) / 100
   const artistsPrice = galleryPrice.value - commission
