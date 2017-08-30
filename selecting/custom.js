285abff2d2eb7a8052974d3c5190dbd316693798
Maurice van Cooten <maurice@minus.nz>
2017-08-30 13:14:10 +1200

    getElementById instead of querySelector

--- selecting/custom.js
+++ selecting/custom.js
@@ -3,18 +3,18 @@
 // Get element by tag name
 document.querySelector('h1').style.backgroundColor = 'pink'
   // Get element by id
-document.querySelector('#foo').style.backgroundColor = 'pink'
+document.getElementById('foo').style.backgroundColor = 'pink'
   // Get element by class name
 document.querySelector('.bar').style.backgroundColor = 'pink'
   // Apply multiple CSS statements at once
-document.querySelector('.quux').style.cssText = 'background: pink; color: red;'
+document.getElementById('baz').style.cssText = 'background: pink; color: red;'
   // Apply a class
-document.querySelector('.baz').classList.add('foo')
+document.getElementById('quux').classList.add('foo')
   // Selecting a specific element within multiple elements
 const p = document.querySelectorAll('p')
 p[4].style.color = 'red'
   // Selecting multiple elements
 const h2 = document.querySelectorAll('h2')
-for (var i = 0; i < h2.length; i++) {
+for (let i = 0; i < h2.length; i++) {
   h2[i].style.color = '#cccccc'
 }
