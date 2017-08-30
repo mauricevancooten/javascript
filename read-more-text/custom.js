df020be7664e5a6c2e9e737a3433d08c574be860
Maurice van Cooten <maurice@minus.nz>
2017-08-30 13:20:10 +1200

    fixed responsive height bug, let instead of var, getElementById instead of querySelector

--- read-more-text/custom.js
+++ read-more-text/custom.js
@@ -1,31 +1,32 @@
 'use strict'
 
-var link, visible
+let link,
+  visible
 
-const more = document.querySelector('.more')
+const more = document.getElementById('more')
 
-  // Hide content with CSS & Add border to accommodate any padding or margins
-more.style.cssText = 'max-height:0; overflow:hidden; border:1px solid transparent;'
-  //  Get height of .more element
-const height = more.scrollHeight
+// Hide content with CSS & Add border to accommodate any padding or margins
+more.style.cssText = 'max-height:0; overflow:hidden;'
 
 link = document.createElement('a')
 link.setAttribute('href', '#')
 link.classList.add('show-more')
 link.innerHTML = 'Read more...'
-  // Insert button after .more element
+// Insert button after .more element
 more.parentNode.insertBefore(link, more.nextSibling)
 
 link = document.querySelector('.show-more');
 
 visible = false
 
-link.addEventListener('click', function(e) {
+link.addEventListener('click', (e) => {
+  //  Get height of .more element
+  const height = more.scrollHeight
   e.preventDefault
   // If visible is set to false
   if (!visible) {
     link.innerHTML = 'Show less...'
-    more.style.cssText = 'transition:1s; overflow:hidden; max-height:' + height +'px;'
+    more.style.cssText = `transition:1s; overflow:hidden; max-height:${height}px;`
   } else {
     link.innerHTML = 'Read more...'
     more.style.maxHeight = 0
