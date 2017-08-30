3e1012a59ce2d466a229ade4e7ca15b236fc3966
Maurice van Cooten <maurice@minus.nz>
2017-08-30 13:18:32 +1200

    string literal, getElementById instead of querySelector, let instead of var

--- scroll-progress-bar/custom.js
+++ scroll-progress-bar/custom.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const scrollBar = document.querySelector('.scroll-bar')
+const scrollBar = document.getElementById('scroll-bar')
 const body = document.querySelector('body')
-const height = body.clientHeight
-var percentage = 0
+const height = body.scrollHeight
+let percentage = 0
 
 scrollBar.style.width = ((window.innerHeight / height)) * 100 + '%'
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', () => {
   percentage = ((this.pageYOffset + this.innerHeight) / height) * 100
-  scrollBar.style.width = percentage + '%'
+  scrollBar.style.width = `${percentage}%`
 })
