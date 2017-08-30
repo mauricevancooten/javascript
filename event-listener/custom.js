fb30d3372e073fa695e3ed0ea68467d5c7a1906b
Maurice van Cooten <maurice@minus.nz>
2017-08-30 13:26:12 +1200

    arrow function, all CSS in JavaScript, get height from element

--- event-listener/custom.js
+++ event-listener/custom.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const el = document.querySelector('h1')
-const btn = document.querySelector('.show')
+const btn = document.getElementById('show')
+const height = el.scrollHeight
 
-el.classList.add('hide-text')
-el.style.transition = '1s'
+el.style.cssText = 'overflow: hidden; transition: 1s; max-height: 0;'
 
 // Listens for click event before triggering script
-btn.addEventListener('click', function() {
-  el.classList.remove('hide-text')
+btn.addEventListener('click', () => {
+  el.style.cssText = `overflow: hidden; transition: 1s; max-height: ${height}px;`
 })
