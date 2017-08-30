c1ea93f95a300e7e242b4e8f181aa72e37cb095d
Maurice van Cooten <maurice@minus.nz>
2017-08-30 13:27:03 +1200

    string literal, getElementById instead of querySelector

--- clock/custom.js
+++ clock/custom.js
@@ -1,20 +1,20 @@
 'use strict'
 
-const time = document.querySelector('.time')
+const time = document.getElementById('time')
 
 function updateTime() {
   const currentTime = new Date()
   const hh = currentTime.getHours()
   const mm = currentTime.getMinutes()
   const ss = currentTime.getSeconds()
-
-  const formattedTime = zeroPadder(hh) + " : " + zeroPadder(mm) + " : " + zeroPadder(ss)
+  // Format time to iprefix 0 if < 10
+  const formattedTime = `${zeroPadder(hh)} : ${zeroPadder(mm)} : ${zeroPadder(ss)}`
 
   time.innerHTML = formattedTime
 }
 
 function zeroPadder(n) {
-	return (parseInt(n, 10) < 10 ? "0" : "") + n
+	return (parseInt(n, 10) < 10 ? '0' : '') + n
 }
 
 updateTime()
