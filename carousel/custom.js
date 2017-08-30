f3f653a568fa779064dfef1a9dfddd50116979ad
Maurice van Cooten <maurice@minus.nz>
2017-08-30 13:31:20 +1200

    string literal, arrow function, fix preventDefault bug, let and const instead of var

--- carousel/custom.js
+++ carousel/custom.js
@@ -1,115 +1,112 @@
 'use strict'
 
-window.onload = function() {
-  
-var slideShow, div, prev, next, width, height, count
+window.onload = () => {
 
-slideShow = document.querySelector('.slide-show')
-div = slideShow.querySelectorAll('div')
+  const slideShow = document.getElementById('slide-show')
+  const div = slideShow.querySelectorAll('div')
 
-// Set the minimum width (slideshow width) and transition to each div
+  // Set the minimum width (slideshow width) and transition to each div
 
-width = slideShow.clientWidth
+  let width = slideShow.clientWidth
 
-for (var i; i < div.length; i++) {
-  div[i].style.cssText = 'min-width:' + width + 'px;'
-}
+  for (let i; i < div.length; i++) {
+    div[i].style.cssText = `min-width: ${width}px;`
+  }
 
-// Set the height of the slideshow to the height of the first div
+  // Set the height of the slideshow to the height of the first div
 
-height = div[0].clientHeight
-slideShow.style.height = height + 'px'
+  let height = div[0].clientHeight
+  slideShow.style.height = `${height}px`
 
-// Set divs in a row
+  // Set divs in a row
 
-count = 0;
+  let count = 0;
 
-for (var i = 0; i < div.length; i++) {
-  div[i].style.left = (i * width) - ((count) * width) + 'px'
-}
+  for (let i = 0; i < div.length; i++) {
+    div[i].style.left = `${ (i * width) - ((count) * width)}px`
+  }
 
-// Prev, next buttons
+  // Prev, next buttons
 
-prev = document.createElement('a'),
-  next = document.createElement('a')
-prev.innerHTML = '< prev'
-next.innerHTML = 'next >'
-prev.classList.add('prev')
-next.classList.add('nxt')
-prev.setAttribute('href', '#')
-next.setAttribute('href', '#')
-slideShow.appendChild(prev)
-slideShow.appendChild(next)
+  let prev = document.createElement('a')
+  let next = document.createElement('a')
+  prev.innerHTML = '< prev'
+  next.innerHTML = 'next >'
+  prev.classList.add('prev')
+  next.classList.add('nxt')
+  prev.setAttribute('href', '#')
+  next.setAttribute('href', '#')
+  slideShow.appendChild(prev)
+  slideShow.appendChild(next)
 
-activeLinks()
+  activeLinks()
 
-next.addEventListener('click', function(e) {
-  e.preventDefault
-  if (count < (div.length - 1)) {
-    count += 1;
-    for (var i = 0; i < div.length; i++) {
-      div[i].style.cssText = 'left:' + ((i * width) - ((count) * width)) + 'px; transition: 1s;'
+  next.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (count < (div.length - 1)) {
+      count += 1;
+      for (let i = 0; i < div.length; i++) {
+        div[i].style.cssText = `left: ${ (i * width) - ((count) * width)}px; transition: 1s;`
+      }
+    }
+    activeLinks()
+  })
+
+  prev.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (count > 0) {
+      count -= 1;
+      for (let i = 0; i < div.length; i++) {
+        div[i].style.cssText = `left: ${ (i * width) - ((count) * width)}px; transition:1s;`
+      }
+    }
+    activeLinks()
+  })
+
+  // Check for prev or next image
+
+  function activeLinks() {
+    // Disable prev button if on first slide
+    if (count === 0) {
+      prev.style.opacity = .5
+      prev.removeAttribute('href', '#')
+    } else {
+      prev.setAttribute('href', '#')
+      prev.style.opacity = 1
+    }
+    // Disable next button if on last slide
+    if (count === (div.length - 1)) {
+      next.style.opacity = .5
+      next.removeAttribute('href', '#')
+    } else {
+      next.setAttribute('href', '#')
+      next.style.opacity = 1
     }
   }
-  activeLinks()
-})
 
-prev.addEventListener('click', function(e) {
-  e.preventDefault
-  if (count > 0) {
-    count -= 1;
-    for (var i = 0; i < div.length; i++) {
-      // div[i].style.left = (i * width) - ((count) * width) + 'px'
-      div[i].style.cssText = 'left:' + ((i * width) - ((count) * width)) + 'px; transition:1s;'
-    }
-  }
-  activeLinks()
-})
+  // Maintain responsive design on window resize
 
-// Check for prev or next image
-
-function activeLinks() {
-  // Disable prev button if on first slide
-  if (count == 0) {
-    prev.style.opacity = .5
-    prev.removeAttribute('href', '#')
-  } else {
-    prev.setAttribute('href', '#')
-    prev.style.opacity = 1
-  }
-  // Disable next button if on last slide
-  if (count == (div.length - 1)) {
-    next.style.opacity = .5
-    next.removeAttribute('href', '#')
-  } else {
-    next.setAttribute('href', '#')
-    next.style.opacity = 1
-  }
-}
-
-// Maintain responsive design on window resize
-
-window.addEventListener('resize', function(e) {
-  e.preventDefault
+  window.addEventListener('resize', (e) => {
+    e.preventDefault
     // Recalculate width
-  width = slideShow.clientWidth
+    width = slideShow.clientWidth
     // Resize each div
-  for (var i = 0; i < div.length; i++) {
-    div[i].cssText = 'transition: 1s; min-width:' + width + 'px;'
+    for (let i = 0; i < div.length; i++) {
+      div[i].cssText = `transition: 1s; min-width: ${width}px`
       // Wait for width before resetting height
-    setTimeout(function() {
+      setTimeout(() => {
         height = div[0].clientHeight
-        slideShow.style.height = height + 'px'
+        slideShow.style.height = `${height}px`
       }, 1000)
       // Wait for height before resetting width
-    setTimeout(function() {
-      width = slideShow.clientWidth
-      div[1].style.left = width + 'px'
-       for (var i = 0; i < div.length; i++) {
-        div[i].style.left = (i * width) - ((count) * width) + 'px'
-      }
-    }, 1500)
-  }
-})
+      setTimeout(() => {
+        width = slideShow.clientWidth
+        div[1].style.left = `${width}px`
+        for (let i = 0; i < div.length; i++) {
+          div[i].style.left = `${ (i * width) - ((count) * width)}px`
+        }
+      }, 1500)
+    }
+  })
 
 }
