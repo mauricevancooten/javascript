68b788e132353796f22a9cf3c7a292dde16ae485
Maurice van Cooten <maurice@kosmo.studio>
2019-07-30 20:38:28 +1200

    preventDefault behaviour

--- smooth-scroll/custom.js
+++ smooth-scroll/custom.js
@@ -6,7 +6,8 @@ for (let i = 0; i < anchorLinks.length; i++) {
 	anchorLinks[i].addEventListener('click', smoothScroll)
 }
 
-function smoothScroll() {
+function smoothScroll(e) {
+	e.preventDefault()
 	const anchor = this.getAttribute('href')
 	const dest = document.querySelector(anchor)
 	window.scroll({ top: dest.offsetTop, behavior: 'smooth' })
